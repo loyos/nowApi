c8eafd95317c81cc291c675446fb95c50c7a9d9c
Loy <loyenrique1@gmail.com>
2018-02-24 11:25:26 +0100

    add promos with image, done!

--- src/app/add-promo/add-promo.component.ts
+++ src/app/add-promo/add-promo.component.ts
@@ -16,13 +16,31 @@ export class AddPromoComponent implements OnInit {
     promoImage: ''
     // img: ''
   };
+
+  filesToUpload: Array<File> = [];
+
   constructor(private madridService: MadridService) { }
 
   ngOnInit() {
   }
 
-  addPromo(){
-    this.madridService.addPromo(this.promo).subscribe(data =>{
+  fileChangeEvent(fileInput: any) {
+    this.filesToUpload = <Array<File>>fileInput.target.files;
+    //this.product.photo = fileInput.target.files[0]['name'];
+  }
+
+  addPromo() {
+
+    const formData: any = new FormData();
+    const files: Array<File> = this.filesToUpload;
+    console.log(files);
+
+    formData.append("promoImage", files[0]);
+    formData.append("name", this.promo.name);
+    formData.append("info", this.promo.info);
+    formData.append("day", this.promo.day);
+    formData.append("category", this.promo.category);
+    this.madridService.addPromo(formData).subscribe(data => {
       console.debug(data);
     });
   }
